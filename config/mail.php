85735b4fa1e43a5ae0ea80d1e69c8cb5d9c01929
agoussec <agoussec@gmail.com>
2020-10-05 15:54:53 +0530

    Mail driver changed to sendmail

--- config/mail.php
+++ config/mail.php
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 2525),
+    'port' => env('MAIL_PORT', 587),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'agoussec@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
