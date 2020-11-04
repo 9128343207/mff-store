beed909d539b995b77ded805733610e7f92feb53
agoussec <agoussec@gmail.com>
2020-11-04 17:41:47 +0530

    Store creation notification added

--- app/Http/Controllers/Vendor/StoreController.php
+++ app/Http/Controllers/Vendor/StoreController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Store;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use App\Notifications\StoreCreated;
 
 class StoreController extends Controller
 {
@@ -21,13 +22,18 @@ class StoreController extends Controller
             'store_name' => 'required|unique:stores|max:255',
             'description' => 'required',
         ]);
+
+        $user = Auth::user();
         $store = new Store();
         $store->store_name = $request->store_name;
-        $store->user_id = Auth::user()->id;
+        $store->user_id = $user->id;
         $store->description = $request->description;
         $store->save();
         DB::table('users')->update(['is_seller' => 1]);
             // dd($store->id); // TODO redirect to dashboard
+
+        $user->notify(new StoreCreated($store));
+
         return redirect()->route('vendor.dashboard');
     }
 }
---
+++ app/Notifications/StoreCreated.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class StoreCreated extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('You have successfully created store in industrialcartsupplycart.com.')
+                    ->action('Visit Your Store', url('/vendor/dashboard'))
+                    ->line('Thank you for using industrialcartsupplycart.com!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
