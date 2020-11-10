93441a7a16d575690049bc7cef9a548ace5be70c
agoussec <agoussec@gmail.com>
2020-11-10 15:38:19 +0530

    name url modified

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,8 @@ Route::post('/products/search', 'HomeController2@ProductSearch')->name('product.
 
 Route::get('/product/{id}', 'Vendor\ProductController@SingleProduct')->name('product.view');
 
-Route::get('/p/{name}', 'Vendor\ProductController@SingleProductNamed')->name('product.named.view'); // for single product
-Route::get('/c/{cat}', 'HomeController2@productByNamedCategory')->name('product.by.category'); // category based
+Route::get('/p/{name?}', 'Vendor\ProductController@SingleProductNamed')->name('product.named.view')->where('name', '(.*)'); // for single product
+Route::get('/c/{cat?}', 'HomeController2@productByNamedCategory')->name('product.by.category')->where('cat', '(.*)'); // category based
 // newleter
 
 Route::post('/add-to-newleter', 'NewsleterController@add');
